b412fde97f1897113577714e473fa9d4f0771c28
PluckySaltyfish <1828151761@qq.com>
2020-06-17 23:53:02 +0800

    add server igonred files

--- FlaskServer/server.py
+++ FlaskServer/server.py
@@ -1,12 +1,18 @@
-from flask import Flask, jsonify, request,send_from_directory,make_response,render_template
+from flask import Flask, jsonify, request,send_from_directory,make_response
 import sys
 def start(host="0.0.0.0",
           port=6666,
           debug=True):
+
     app = Flask(__name__)
     @app.route('/')
     def index():
         return 'hello world'
+    
+    @app.route('/record', methods=['POST'])
+    def record():
+        print(request.data)
+        return jsonify({'hi': 1, 'hello': 2})
 
     app.run(debug=debug, host=host, port=port, use_reloader=False,threaded=True)
 if __name__ == "__main__":
